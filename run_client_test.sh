d3484358412cba8839a6f3db45ee3460f35caebc
Jeff Campbell <cjeffrey@blackduck.com>
2026-08-05 16:07:40 -0700

    Add files via upload

--- run_client_test.sh
+++ run_client_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ./source_bearer_demo.sh
+
 curl -s -X POST http://127.0.0.1:8080/proxy \
 -H 'Content-Type: application/json' \
 -d '{"prompt":"add a main routine to the end of attached code and return the updated complete file"}' \
