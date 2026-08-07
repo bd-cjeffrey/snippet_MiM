028590ed82a79137f861bba62997c0af8c18ba36
Jeff Campbell <cjeffrey@blackduck.com>
2026-08-07 08:21:11 -0700

    Add files via upload

--- run_server.sh
+++ run_server.sh
@@ -2,6 +2,8 @@
 
 source ./source_bearer_demo.sh
 
+export PYTHONWARNINGS="ignore:Unverified HTTPS request"
+
 #./.venv/bin/python  mim_proxy.py -l info -m claude-opus-4-7
-./.venv/bin/python  mim_proxy.py -l debug
+./.venv/bin/python  mim_proxy.py -l info  -m gpt-4o-mini
 #./.venv/bin/python  mim_proxy.py -l debug 
