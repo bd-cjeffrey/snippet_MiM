47ffdc24b4a92fad04076d2e033b057b49660454
Jeff Campbell <cjeffrey@blackduck.com>
2026-08-07 10:55:51 -0700

    Add files via upload

--- test_snippet_match.sh
+++ test_snippet_match.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# 
+# Show snippet matching output, both the fingerprint that is generated,
+# and the set of license matches found against what is in the test_code.c 
+# file.
+#
 
 set -x
 
